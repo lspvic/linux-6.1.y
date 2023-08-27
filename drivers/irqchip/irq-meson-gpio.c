79afbe28a77f62ebbd567afe2c3eb116be8772d7
Luigi Capriotti <lcapriotti@users.noreply.github.com>
2023-08-27 17:46:03 +0200

    Fix missing trailing brackets

--- drivers/irqchip/irq-meson-gpio.c
+++ drivers/irqchip/irq-meson-gpio.c
@@ -152,6 +152,7 @@ static const struct meson_gpio_irq_params s4_params = {
 
 static const struct meson_gpio_irq_params sc2_params = {
 	INIT_MESON_S4_COMMON_DATA(87)
+};
 
 static const struct of_device_id meson_irq_gpio_matches[] __maybe_unused = {
 	{ .compatible = "amlogic,meson8-gpio-intc", .data = &meson8_params },
